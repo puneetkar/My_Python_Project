15640404ec4dfc33a5e8b88e1d1d0956577c744a
puneetkar <puneetkarmaveer@gmail.com>
2023-02-17 09:15:59 +0530

    new

---
+++ index1.py
@@ -0,0 +1,5 @@
+s=input('enter the some string')
+i=0
+for x in s:
+    print('The charactore present in ',i, 'index of ',x)
+    i=i+1
---
+++ pytho.py
@@ -0,0 +1,11 @@
+s='python is very easy to learn'
+l=s.split()
+l1=[]
+i=0
+output=''
+while i<len(s):
+    for i%2 ==0:
+    l1.append[l[i]]
+else:
+    l1.append([l[i]][::-1])
+    print(l1)
